db35656dce0b401319b94f0b7945036103a8c97a
Masatoshi N <nishiguchi.masa@gmail.com>
2022-01-29 16:34:42 -0500

    Use TypedStruct for BH1750

--- lib/bh1750.ex
+++ lib/bh1750.ex
@@ -3,23 +3,36 @@ defmodule BH1750 do
   Documentation for `BH1750`.
   """
 
-  @i2c_address1 0x23
+  @default_bus_name "i2c-1"
+  @default_address 0x23
+  @default_mode :continuous
+  @default_resolution :high
   @cmd_power_down 0b0000_0000
 
-  defstruct [:config, :transport]
+  @type lux :: 0..65535
+
+  use TypedStruct
+
+  typedstruct do
+    @typedoc "The BH1750 sensor"
+
+    field(:config, BH1750.Config.t(), enforce: true)
+    field(:transport, BH1750.Transport.t(), enforce: true)
+  end
 
   @doc """
   ## Examples
 
-      bh = BH1750.new
-      BH1750.read(bh)
+  bh = BH1750.new
+  BH1750.read(bh)
 
   """
+  @spec new(keyword | map) :: BH1750.t()
   def new(opts \\ []) do
-    bus_name = Access.get(opts, :bus_name, "i2c-1")
-    address = Access.get(opts, :address, @i2c_address1)
-    mode = Access.get(opts, :mode, :continuous)
-    resolution = Access.get(opts, :resolution, :high)
+    bus_name = Access.get(opts, :bus_name, @default_bus_name)
+    address = Access.get(opts, :address, @default_address)
+    mode = Access.get(opts, :mode, @default_mode)
+    resolution = Access.get(opts, :resolution, @default_resolution)
 
     %__MODULE__{
       config: BH1750.Config.new(mode, resolution),
@@ -28,11 +41,13 @@ defmodule BH1750 do
     |> tap(&power_down/1)
   end
 
-  def power_down(%{transport: transport}) do
+  @spec power_down(BH1750.t()) :: :ok | {:error, any}
+  def power_down(%__MODULE__{transport: transport}) do
     transport.write_fn.([@cmd_power_down])
   end
 
-  def read(%{config: config, transport: transport}) do
+  @spec read(BH1750.t()) :: {:ok, lux} | {:error, any}
+  def read(%__MODULE__{config: config, transport: transport}) do
     with {:ok, <<msb, lsb>>} <- transport.write_read_fn.([config.opcode], 2) do
       {:ok, BH1750.Calc.raw_to_lux(<<msb, lsb>>)}
     end
