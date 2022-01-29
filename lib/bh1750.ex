26e3cea85b1fefb35b49a7d78ed201026c127ed5
Masatoshi N <nishiguchi.masa@gmail.com>
2022-01-29 17:04:06 -0500

    Sleep between write and read; rename read/1 to measure/1

--- lib/bh1750.ex
+++ lib/bh1750.ex
@@ -1,12 +1,13 @@
 defmodule BH1750 do
   @moduledoc """
-  Documentation for `BH1750`.
+  Use BH1750 16-bit ambient light sensor in Elixir.
   """
 
   @default_bus_name "i2c-1"
   @default_address 0x23
   @default_mode :continuous
   @default_resolution :high
+
   @cmd_power_down 0b0000_0000
 
   @type lux :: 0..65535
@@ -21,11 +22,7 @@ defmodule BH1750 do
   end
 
   @doc """
-  ## Examples
-
-  bh = BH1750.new
-  BH1750.read(bh)
-
+  Instantiates a BH1750 sensor.
   """
   @spec new(keyword | map) :: BH1750.t()
   def new(opts \\ []) do
@@ -41,14 +38,22 @@ defmodule BH1750 do
     |> tap(&power_down/1)
   end
 
+  @doc """
+  Powers down the BH1750 sensor.
+  """
   @spec power_down(BH1750.t()) :: :ok | {:error, any}
   def power_down(%__MODULE__{transport: transport}) do
     transport.write_fn.([@cmd_power_down])
   end
 
-  @spec read(BH1750.t()) :: {:ok, lux} | {:error, any}
-  def read(%__MODULE__{config: config, transport: transport}) do
-    with {:ok, <<msb, lsb>>} <- transport.write_read_fn.([config.opcode], 2) do
+  @doc """
+  Measures the ambient light.
+  """
+  @spec measure(BH1750.t()) :: {:ok, lux} | {:error, any}
+  def measure(%__MODULE__{config: config, transport: transport}) do
+    with :ok <- transport.write_fn.([config.opcode]),
+         :ok <- Process.sleep(config.measurement_time),
+         {:ok, <<msb, lsb>>} <- transport.write_read_fn.([config.opcode], 2) do
       {:ok, BH1750.Calc.raw_to_lux(<<msb, lsb>>)}
     end
   end
