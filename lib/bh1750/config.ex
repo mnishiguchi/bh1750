1c4723943134a229f8d09d30b06c365de2eda12b
Masatoshi N <nishiguchi.masa@gmail.com>
2022-01-29 16:25:13 -0500

    Use TypedStruct for Config

--- lib/bh1750/config.ex
+++ lib/bh1750/config.ex
@@ -1,13 +1,21 @@
 defmodule BH1750.Config do
   @moduledoc false
 
-  defstruct [
-    :measurement_time,
-    :mode,
-    :opcode,
-    :resolution
-  ]
+  use TypedStruct
 
+  @type mode :: :continuous | :one_time
+  @type resolution :: :high | :high2 | :low
+
+  typedstruct do
+    @typedoc "The configuration settings for the BH1750 sensor"
+
+    field(:measurement_time, non_neg_integer, enforce: true)
+    field(:mode, mode, enforce: true)
+    field(:opcode, byte, enforce: true)
+    field(:resolution, resolution, enforce: true)
+  end
+
+  @spec new(mode, resolution) :: BH1750.Config.t()
   def new(mode, resolution) do
     %__MODULE__{
       mode: mode,
@@ -17,6 +25,7 @@ defmodule BH1750.Config do
     }
   end
 
+  @spec opcode(mode, resolution) :: byte
   def opcode(:continuous, :high), do: 0b0001_0000
   def opcode(:continuous, :high2), do: 0b0001_0001
   def opcode(:continuous, :low), do: 0b0001_0011
@@ -24,6 +33,7 @@ defmodule BH1750.Config do
   def opcode(:one_time, :high2), do: 0b0010_0001
   def opcode(:one_time, :low), do: 0b0010_0011
 
+  @spec measurement_time(resolution) :: 24 | 180
   def measurement_time(:high), do: 180
   def measurement_time(:high2), do: 180
   def measurement_time(:low), do: 24
