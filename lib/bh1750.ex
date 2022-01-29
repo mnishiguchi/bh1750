60abff834ad35e46e3f5a519d4bdf447f92d886c
Masatoshi N <nishiguchi.masa@gmail.com>
2022-01-28 21:59:09 -0500

    Initial

---
+++ lib/bh1750.ex
@@ -0,0 +1,18 @@
+defmodule Bh1750 do
+  @moduledoc """
+  Documentation for `Bh1750`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Bh1750.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/bh1750_test.exs
@@ -0,0 +1,8 @@
+defmodule Bh1750Test do
+  use ExUnit.Case
+  doctest Bh1750
+
+  test "greets the world" do
+    assert Bh1750.hello() == :world
+  end
+end
